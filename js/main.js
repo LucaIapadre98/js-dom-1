1f7158a5a6fab107a3f06d90b2af8da3b591c38d
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-09 17:52:48 +0200

    aggiunto modifiche

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
+function myFunction() {
+    document.getElementById("myImg").src = "./img/yellow_lamp.png";
+  }
 
-const imgEl = document.querySelector(".my-img");
-imgEl.className="my-img";
-imgEl.src="./img/white_lamp.png";
-imgEl.alt="immagine-1";
-console.log(imgEl.className);
-console.log(imgEl.src);
-console.log(imgEl.alt);
+
+
+
+
 
 
 
