40764f01c4643da609208e24282579006cd2bc87
LucaIapadre98 <luca.iap.1998@gmail.com>
2025-04-09 15:29:00 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+const titleEl = document.getElementById ("main-title");
+titleEl.innerHTML = `Titolo by <span class="text-warning">Java</span>`
+
+console.log(titleEl);
+
+// titleEl.classList.remove("bg-danger");
+
+//titleEl.classList.add("bg-success");
+
+
+
+ 
